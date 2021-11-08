f4481f4feb3434b502d4a7eb9aaec85030a3f37c
guru2711 <59472229+guru2711@users.noreply.github.com>
2021-11-08 16:42:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 const Url = require("./modal/schema")
-
+require("dotenv").config()
 const app = express()
 
 app.use(cors())
